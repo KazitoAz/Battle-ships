3ea6e02f2fb0a76bcf287c031714e267f6869255
Ang Yu Fan <nickyufan@gmail.com>
2018-04-20 22:03:06 +1000

    add new GameTemplate file

---
+++ GameTemplate/src/Model/AIPlayer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwinGameSDK;
+
+namespace MyGame
+{
+    public abstract class AIPlayer
+    {
+        protected class Location
+        {
+            private int _Row;
+            private int _Column;
+
+            public int Row
+            {
+                get
+                {
+                    return _Row;
+                }
+                set
+                {
+                    _Row = value;
+                }
+            }
+
+            public int Column
+            {
+                get
+                {
+                    return _Column;
+                }
+                set
+                {
+                    _Column = value;
+                }
+            }
+
+            public Location(int row, int column)
+            {
+                _Column = column;
+                _Row = row;
+            }
+
+            public AIPlayer(int row, int column)
+            {
+                _Column = column;
+                _Row = row;
+            }
+        }
+
+
+
+
+
+        public AIPlayer(BattleShipsGame game) : base(game)
+        {
+        }
+
+        protected override void GenerateCoords(ref int row, ref int column)
+        {
+        }
+
+
+        protected override void ProcessShot(int row, int col, AttackResult result)
+        {
+        }
+
+
+        public override AttackResult Attack()
+        {
+            AttackResult result;
+            int row = 0;
+            int column = 0;
+            for (
+            ; ((result.Value != ResultOfAttack.Miss)
+                        && ((result.Value != ResultOfAttack.GameOver)
+                        && !SwinGame.WindowCloseRequested));
+            )
+            {
+                Delay();
+                GenerateCoords(row, column);
+                // generate coordinates for shot
+                result = _game.Shoot(row, column);
+                // take shot
+                ProcessShot(row, column, result);
+            }
+
+            return result;
+        }
+
+        private void Delay()
+        {
+            int i;
+            for (i = 0; (i <= 150); i++)
+            {
+                // Dont delay if window is closed
+                if (SwinGame.WindowCloseRequested)
+                {
+                    return SwinGame.Delay(5);
+                }
+
+                SwinGame.ProcessEvents();
+                SwinGame.RefreshScreen();
+            }
+
+        }
+    }
+}
---
+++ GameTemplate/src/Model/Tile.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Tile knows its location on the grid, if it is a ship and if it has been shot before
+namespace MyGame
+{
+    public class Tile
+    {
+        private readonly int _RowValue;         //the row value of the tile
+        private readonly int _ColumnValue;      //the column value of the tile
+        private Ship _Ship = null;              //the ship the tile belongs to
+        private bool _Shot = false;             //the tile has been shot at
+
+        //Has the tile been shot
+        //<value>indicate if the tile has been shot</value>
+        //<returns>true if the tile was shot</returns>
+        public bool Shot
+        {
+            get
+            {
+                return _Shot;
+            }
+            set
+            {
+                _Shot = value;
+            }
+        }
+
+        public int Row => _RowValue;
+
+        public int Column => _ColumnValue;
+
+        public Ship Ship
+        {
+            get
+            {
+                return _Ship;
+            }
+            set
+            {
+                if (_Ship == null)
+                {
+                    _Ship = value;
+                    if (value != null)
+                    {
+                        _Ship.AddTile(this);
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException("There is already a ship at [" + Row + ", " + Column + "]");
+                }
+            }
+        }
+        // The tile constructor will know where it is on the grid, and is its a ship
+        //<param name = "row" > the row on the grid</param>
+        //<param name="col">the col on the grid</param>
+        //<param name="ship">what ship it is</param>
+        public Tile(int row, int col, Ship ship)
+        {
+            _RowValue = row;
+            _ColumnValue = col;
+            _Ship = ship;
+        }
+
+        public void ClearShip()
+        {
+            _Ship = null;
+        }
+
+        public TileView View
+        {
+            get
+            {
+                if (_Ship == null)
+                {
+                    if (_Shot)
+                    {
+                        return TileView.Miss;
+                    }
+                    else
+                    {
+                        return TileView.Sea;
+                    }
+                }
+                else
+                {
+                    if ((_Shot))
+                    {
+                        return TileView.Hit;
+                    }
+                    else
+                    {
+                        return TileView.Ship;
+                    }
+                }
+            }
+        }
+        /*Shoot allows a tile to be shot at, and if the tile has been hit before
+        it will give an error*/
+        internal void Shoot()
+        {
+            if ((false == Shot))
+            {
+                Shot = true;
+                if (_Ship != null)
+                {
+                    _Ship.Hit();
+                }
+            }
+            else
+            {
+                throw new ApplicationException("You have already shot this square");
+            }
+        }
+    }
+}
