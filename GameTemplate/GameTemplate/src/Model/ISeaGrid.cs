4a38ccd8de4969b59a787ee8a22b6186e7759b6a
Ang Yu Fan <nickyufan@gmail.com>
2018-05-04 16:30:30 +1000

    new version

---
+++ GameTemplate/GameTemplate/src/Model/ISeaGrid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame.src.Model
+{
+    // '' <summary>
+    // '' The ISeaGrid defines the read only interface of a Grid. This
+    // '' allows each player to see and attack their opponents grid.
+    // '' </summary>
+    public interface ISeaGrid
+    {
+        int Width
+        {
+        }
+    }
+}
